686307aeb6396e0cd06d305f4c665b89e4f7893d
yoninazarathy <yoninazarathy@gmail.com>
2021-09-06 07:35:23 +1000

    factor

--- src/polynomial_factorization/factor.jl
+++ src/polynomial_factorization/factor.jl
@@ -1,44 +1,14 @@
-#Fermat's little theorem
-# a^p is congruent to to a mod p (for p prime)
-# a^p-a = 0 mod p
-# x^p-x = 0 mod p  True for any x
-# Hence the factorization of the "special" polynomial x^p-x is known:  (x-0)(x-1)(x-2)...(x-(p-1))
+#############################################################################
+#############################################################################
+#
+# This file defines the polynomial type with several operations 
+#                                                                               
+#############################################################################
+#############################################################################
 
-# A polynomial has a root a
-#QQQQ - Unit test (Yoni do....)
-
-#Say we want to factor F
-#That means we want f_1,...,f_k irreducible polynomials such that f_1^n_1* ... *f_k^n_k = F modulo prime
-#The factorization process will initially give us f_1,...,f_k  (but not the powers...) so we do it 
-
-#reconsider again the cyclotonic polynomial x^p-x (provided p is an odd prime, thus p-1 is even).
-
-#x^p-x = x(x^(p-1) - 1) note that p-1 is even 
-            # = x(x^(p-1)/2 - 1)(x^(p-1)/2 + 1)
-
-#So the product of linear factors should factor into the (triple product) above 
-
-#(x^p)^k-x is the product of all monic irreducible polynomials in Z_p(x) of degree d / k 
-
-# function factor(f::Polynomial, prime::Int)
-    
-#     #Step 1 - remove the content of the polynomial 
-#     f = prim_part(f)
-
-#     #Step 2 - remove repeated roots (make the polynomial square free)
-#     f = square_free(f)
-
-#     #QQQQQ - Assume here we are square_free....
-
-#     #Let's work on getting the linear factors
-#     #This g is the product of all linear factors of f
-#     g = gcd(f, cyclotonic_polynomial(prime), prime)
-# end
-
-# function factor_recusrive_clean(f::Polynomial,prime::Int)
-#     if STOPPING CONDITION  -  
-# end
-
+####################################
+# Polynomial type and construction #
+####################################
 
 """
 Factors a polynomial over the field Z_p.
@@ -46,7 +16,23 @@ Factors a polynomial over the field Z_p.
 Returns a list of irreducible polynomials (mod p) such that their product of the list (mod p) is f. Irreducibles are fixed points on the function factor.
 """
 function factor(f::Polynomial, prime::Int) 
-
+    #cantor_zassenhaus_factorization
+    f = mod(f,prime)
+    old_content = content(f)
+    f = mod(f ÷ old_content,prime)      # make f primitive;
+    old_coeff = leading(f).coeff
+    f = mod(f ÷ old_coeff,prime)        # make f monic
+    squares = gcd(f, derivative(f), prime) 
+    sqrfree_f = (f ÷ squares)(prime)    # make f square-free
+    dds = dd_factor(sqrfree_f, prime)
+    # look = map(x -> Factor(x) mod p, dds); 
+#     irreduc_factors := foldr(list_concat, seq(dd_split(dds[k], k, p), k=1..nops(dds)));
+#     # construct the answer
+#     ans := [old_content*old_coeff, 1];  # the old leading coefficient
+#     for k from 1 to nops(irreduc_factors) do:   # nops is length
+#         ans := ans, [irreduc_factors[k], repeated_remainder(f, irreduc_factors[k], p)];
+#     end do;
+#     return [ans];
 end
 
 """
